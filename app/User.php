2e4857761b5b536f669caa263c629a0706f39434
Hayk Karapetyan <haykokalipsis@gmail.com>
2018-12-06 15:04:43 +0400

    Create methods for learning a lesson (83, 84, 85, 86)

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Redis;
 
 /**
  * App\User
@@ -66,4 +67,23 @@ class User extends Authenticatable
     {
         return in_array($this->email, config('myConfig.administrators'));
     }
+    
+    public function completeLesson($lesson)
+    {
+        // We store something like user:5|series:55 = 4
+        Redis::sadd("user:{$this->id}|series:{$lesson->series->id}", $lesson->id);
+    } 
+    
+    public function percentageCompletedForSeries($series)
+    {
+        $numberOfLessonsInSeries = $series->Lessons->count();
+        $numberOfCompletedLessons = $this->getNumberOfCompletedLessonsForSeries($series);
+
+        return ($numberOfCompletedLessons / $numberOfLessonsInSeries) * 100;
+    } 
+    
+    public function getNumberOfCompletedLessonsForSeries($series)
+    {
+        return count(Redis::smembers("user:{$this->id}|series:{$series->id}") );
+    } 
 }
