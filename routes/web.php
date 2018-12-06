e1dbed2b2e0ff0e2188e6eb55894c97f5031d4e3
Hayk Karapetyan <haykokalipsis@gmail.com>
2018-12-06 13:50:45 +0400

    Install Redis (82)

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,29 @@ Route::get('/logout', function () {
     auth()->logout();
 });
 
-Route::get('/', 'FrontendController@welcome');
+//Route::get('/', 'FrontendController@welcome');
 
 
+Route::get('/redis', function() {
+/*
+    Redis has 3 datatypes.
+    1. key: value // string. Super simple, one string value
+       Redis::set('friend', 'fufu');
+       dd(Redis::get('friend'));
 
+    2. key: value // list. An array. Can have duplicate values.
+       Redis::lpush('frameworks', ['vue', 'laravel']); create key if not exists in memory, and add values.
+       dd(Redis::lrange('frameworks', 0, 5)); // get values by offsets. From 0 to 5
+       dd(Redis::lrange('frameworks', 0, -1)); // get values by offsets. From 0 to all
+
+    3. key: value // A Set. No duplicate values. Good for unique values, like ip addresses, or our case
+       Redis::sadd('frontend', ['angular', 'ember']);
+       dd(Redis::smembers('frontend'));
+*/
+
+    Redis::sadd('frontend', ['angular', 'ember']);
+    dd(Redis::smembers('frontend'));
+});
 
 
 Route::get('/register/confirm', 'ConfirmEmailController@index')->name('confirm-email');
